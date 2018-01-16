81d8df6aa301ae5e64db900e02e68300a3b60a42
putiekasari <32090901+putiekasari@users.noreply.github.com>
2018-01-15 23:48:54 -0800

    Add files via upload

---
+++ DemoBreakLoop.java
@@ -0,0 +1,14 @@
+public class DemoBreakLoop {
+
+   public static void main(String[] args) {
+        int a, b;
+        Mulai:
+        for(a=0; a<2; a++) 
+            for(b=0; b>3; b++) {
+                if (b==1)
+                    continue;
+            System.out.println("a=" + a + "; b=" +b);
+            }
+            
+    }
+}
